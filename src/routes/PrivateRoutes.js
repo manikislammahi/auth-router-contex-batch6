fcbf77c7fe3071bb63334e6ff503701ced6e1348
MagicManik <manikislam1952@gmail.com>
2023-01-25 12:15:08 +0600

    code updated

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -5,14 +5,14 @@ import { AuthContext } from '../contex/UserContex';
 const PrivateRoutes = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
 
+    if (loading) {
+        return <div>Loading..</div>
+    }
+
     // ইউজার, ইউজারের uid এবং ইউজারের ইমেইল আইডি ভেরিফাই হলে children এ ঢুকতে দিবো।
     // if (user && user?.uid && user?.emailVerified) {
     //     return children;
     // }
-
-    if (loading) {
-        return <div>Loading..</div>
-    }
 
     // ইউজার এবং ইউজারে uid থাকলে children এ ঢুকতে দিবো।
     if (user && user?.uid) {
